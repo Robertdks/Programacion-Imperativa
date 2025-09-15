0cf4fccd7dabf82d47311cbf94209bf2eb36a1ac
Robertdks <roberanuel@gmail.com>
2025-09-14 21:44:36 -0300

    Ejercicio 1

---
+++ TP1/EJ1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    int numero = 0;
+    char caracter ;
+
+    cout << "Ingrese un numero: " ;
+    cin >> numero ;
+    cout << "Ingrese un caracter: ";
+    cin >> caracter ;
+    for(int i = 0; i < numero ;i++)
+    {
+        for(int j = 0; j <= i ; j++)
+        {
+            cout << caracter ;
+        }
+        cout << endl;
+    }
+    return 0 ;
+    }
